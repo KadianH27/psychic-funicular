6dd4843f901337766700cb6f187bb15d1d01bb2c
KadianH27 <Kadianhall27@gmail.com>
2024-08-08 00:38:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let word = "frog";
+let number = 27;
+let calculatedNumber = number * 7;
+let isMagic = true;
+let name = prompt("frog tale:");
+
+let story = `There lived a ${frog} who loved the number ${27}. ` 
+    `The ${frog} had ${20} lilypads but wanted 7 more lilypads to reach his favorite number. ` 
+    `It was ${true} that the ${frog}'s tale continued with the help of ${frog}.`;
+
+console.log(story);
